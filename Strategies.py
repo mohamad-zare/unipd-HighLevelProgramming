034472eadf8fc66063962210188cb86f56ca4fe6
Mohammad <mohammadmehdi.zare@studenti.unipd.it>
2022-04-30 21:11:20 +0200

    strategies update

--- Strategies.py
+++ Strategies.py
@@ -4,31 +4,31 @@ import random
 
 
 ###### Nice guy strategy: He/She always cooperate.
-def nice_guy(self):
+def nice_guy():
     return True
 
 
 ###### Bad guy strategy: He/She always defect.
-def bad_guy(self):
+def bad_guy():
     return False
 
 
 ###### Mainly bad strategy: He/She defect 75% times. (K is equal to 75 in our case)
 def mainly_bad():
-    c = random.choices([True, False], [0.25, 0.75])
-    return c
+    return random.choices([True, False], [0.25, 0.75])[0]
+    
 
 
 ###### Mainly nice strategy: He/She cooperate 75% times. (K is equal to 25 in our case)
 def mainly_nice():
-    c = random.choices([True, False], [0.75, 0.25])
-    return c
+    return random.choices([True, False], [0.75, 0.25])[0]
+    
 
 
 ###### Tit for Tat strategy: He/She starts with cooperating then repeat what the opponent has done in the previous move.
-def tit_for_tat(n_rounds, opponent_strategy):   
-    if n_rounds == 1:
+def tit_for_tat(n_rounds, strategy2):   
+    if n_rounds == 0:
         c = True
     else:
-        c = opponent_strategy
+        c = strategy2
     return c
